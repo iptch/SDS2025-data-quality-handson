88f0d36def8f1900c5209fb99054a08c68220027
Gian Hiltbrunner <gian.silvan.hiltbrunner@gmail.com>
2025-03-31 16:59:36 +0200

    fixed data

---
+++ data/0_spring_2011/_create_table.sql
@@ -0,0 +1,24 @@
+create table bike_rental
+(
+    id         integer,
+    dteday     text,
+    season     text,
+    yr         integer,
+    mnth       integer,
+    hour       integer,
+    holiday    integer,
+    weekday    integer,
+    workingday integer,
+    weathersit integer,
+    temp       double,
+    atemp      double,
+    hum        double,
+    windspeed  double,
+    casual     integer,
+    registered integer,
+    total      integer,
+    felt_temp  double,
+    humidity   text,
+    year       text,
+    weather    text
+);
