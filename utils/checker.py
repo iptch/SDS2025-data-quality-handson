eaf3716d93b3a9fdd347d88f2b8892f4455c72ba
Gian Hiltbrunner <gian.silvan.hiltbrunner@gmail.com>
2025-04-10 11:15:16 +0200

    simplify first task

--- utils/checker.py
+++ utils/checker.py
@@ -2,22 +2,24 @@ from loguru import logger
 from great_expectations.core.expectation_validation_result import ExpectationValidationResult
 
 def check(task: int, result: ExpectationValidationResult) -> None:
-    assert isinstance(task, int)
-    assert isinstance(result, ExpectationValidationResult)
+    assert isinstance(task, int), "Task must be an integer."
+    assert isinstance(result, ExpectationValidationResult), "Result must be an instance of ExpectationValidationResult."
 
     match task:
         case 1:
             if not result["success"]:
-                logger.error("The expectation was not met, check the result.")
+                logger.error("The expectation was not met, check again.")
                 return
-            if result["result"]["unexpected_count"] != 0:
-                logger.error("The unexpected count is not zero.")
+            if result["expectation_config"]["type"] != "expect_column_to_exist":
+                logger.error("The expectation type is not correct, check again.")
+                return
+            if result["expectation_config"]["kwargs"]["column"] != "season":
+                logger.error("The column name is not correct, check again.")
                 return
 
         case _:
             logger.error(f"Unknown task: {task}. Please provide a valid task number.")
             return
 
-    logger.success("Great job! The result is as expected.")
-
+    logger.success("Great job! The result of the expectation is correct. Continue with the next task.")
 
